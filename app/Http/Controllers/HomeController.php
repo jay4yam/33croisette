8e0a29a2a4ff6e6d4a6a6a432e974c3f73d7ed6c
jayben <jay.ayamee@gmail.com>
2024-09-25 11:49:29 +0200

    update new route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,6 +18,11 @@ class HomeController extends Controller
         return view('welcome');
     }
 
+    public function test()
+    {
+        return view('welcome');
+    }
+
     public function sendRequest(RequestInformation $request)
     {
         try{
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 //affiche la home page du site
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/test', [\App\Http\Controllers\HomeController::class, 'test'])->name('test');
+
 //gère le formulaire de contact
 Route::post('send/request', [\App\Http\Controllers\HomeController::class, 'sendRequest'])->name('send.request');
 
