fde6dfe0c6bfa6e2f14ef11550ea5db52bde758c
jayben <jay.ayamee@gmail.com>
2025-01-21 18:05:24 +0100

    update new route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 //affiche la home page du site
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//import excel
 Route::get('/import', [\App\Http\Controllers\PropertyImportController::class, 'index'])->name('import.index');
 Route::post('/import', [\App\Http\Controllers\PropertyImportController::class, 'import'])->name('import.request');
 
