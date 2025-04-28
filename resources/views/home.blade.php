4b4a9b85dd223ed1b741accb5b7ad0bb6e9de261
jayben <jay.ayamee@gmail.com>
2025-04-28 15:44:39 +0200

    update website v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -507,7 +507,7 @@
                     trigger: '#view',
                     pin: true, // pin the trigger element while active
                     start: 'top top', // when the top of the trigger hits the top of the viewport
-                    end: '+=100%', // end after scrolling 800px beyond the start
+                    end: '+=400', // end after scrolling 400px beyond the start
                     scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
                     once:true,
                     snap: {
