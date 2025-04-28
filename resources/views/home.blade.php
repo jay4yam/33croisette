a84cb995edfa6f57d4afbc80b9f3d3a35ffdcecf
jayben <jay.ayamee@gmail.com>
2025-04-28 17:37:18 +0200

    update website v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -594,7 +594,7 @@
                     trigger: '#kitchen',
                     pin: true, // pin the trigger element while active
                     start: 'top top', // when the top of the trigger hits the top of the viewport
-                    end: '+=800', // end after scrolling 400px beyond the start
+                    end: '+=400', // end after scrolling 400px beyond the start
                     scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
                     once:true,
                     snap: {
