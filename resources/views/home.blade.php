5df50a3a6839767ae5cd00daac24f0f463ffc7dd
jayben <jay.ayamee@gmail.com>
2025-04-28 16:44:45 +0200

    update website v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -216,7 +216,7 @@
         <!-- end video -->
 
         <!-- view -->
-        <section id="view" class="px-6 md:px-0 py-6">
+        <section id="view" class="h-dvh px-6 md:px-0 py-6">
             <div  class="flex items-end justify-around gap-12">
                 <div id="img_left" class="hidden md:block">
                     <img class="h-[calc(100svh-40px)]" loading="lazy" src="{{ asset('images/apartments/view-1.webp') }}" alt="front of the building">
